c30c8bae482332c2e54b1ab9c9c28c5f55c5a07e
Abhishek Wilson <Abhishek.Wilson@neudesic.com>
2022-04-06 01:07:41 +0530

    Initial Push

---
+++ E-Commerce-ApiAndData/DataAccess/Entities/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Entities
+{
+    public class Customer
+    {
+
+        public int CustomerId { get; set; }
+
+        public string CustomerEmail { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public int Phone { get; set; }
+
+        public string? Address { get; set; }
+
+        public int Wallet { get; set; }
+    }
+}
